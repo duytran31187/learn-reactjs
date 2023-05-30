3d2e3127124b947716ab46b12e451889e77096fd
Duy Tran <duy.tranquoc@gforces.auto>
2023-05-31 06:27:35 +0700

    props as the only param of function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) { // jsx just use only one params: props. not as js function(data,title, amount)
     // rule: only one root element
     return (
         <div className='expense-item'>
-            <div>2023-03-15</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>259.29 vnd</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     )
