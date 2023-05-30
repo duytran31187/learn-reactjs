20a4173acc09fe9ecb9879a608edac1397c37bea
Duy Tran <duy.tranquoc@gforces.auto>
2023-05-31 06:07:22 +0700

    just only one root on each component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
 function ExpenseItem() {
-    return <h2>Expense item ! </h2>;
+    // rule: only one root element
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>Title</h2>
+                <div>Amount</div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
