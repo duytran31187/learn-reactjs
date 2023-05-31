220355c3051322a9ddce6cafc27a4b29033c1fdd
Duy Tran <duy.tranquoc@gforces.auto>
2023-05-31 21:44:27 +0700

    dollar symbol

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,13 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => { // jsx just use only one params: props. not as js function(data,title, amount)
     // rule: only one root element
-    
+    // do not confuse: $ is dollar
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <Card className='expense-item__price'>{props.amount}</Card>
+                <Card className='expense-item__price'>${props.amount}</Card>
             </div>
         </Card>
     )
