b4c9a2549d32a28aa2aca75e04efa8be352b7ce8
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-01 06:39:09 +0700

    s5: one state instead of multi states

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -3,21 +3,35 @@ import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
+  const [userInput, setUserInput] = useState({
+
+    'enteredTitle': '',
+    'enteredAmount': '',
+    'enteredDate': '',
+  });
+
   const titleChangeHandler = (event) => {
     console.log('Title changed !!!', event.target.value);
-    setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value
+    });
   };
   const amountChangeHandler = (event) => {
     console.log('amount changed !!!', event.target.value);
-    setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value
+    });
   };
   const dateChangeHandler = (event) => {
     console.log('date changed !!!', event.target.value);
-    setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value
+    });
   };
+
   return (
     <form>
       <div className='new-expense__controls'>
