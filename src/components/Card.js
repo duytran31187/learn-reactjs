49a9fbabe9cbe2933a68e2c148f320aa9dbc743c
Duy Tran <duy.tranquoc@gforces.auto>
2023-05-31 21:03:07 +0700

    note props.children

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import './Card.css';
 
 function Card(props) {
     const classes = 'card ' + props.className; // just get all other classes to this
+    // // props.children => without this, it can render content to Card
     return (
         <div className={classes}>
             {props.children}
