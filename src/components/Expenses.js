2d693497812e738fc35d5168d5e7eeb8bf027c50
Duy Tran <duy.tranquoc@gforces.auto>
2023-05-31 21:15:22 +0700

    loop array

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,9 +5,10 @@ import Card from './Card';
 function Expenses(props) {
     return (
         <Card className="expenses">
-            <ExpenseItem date={props.items[0].date} title={props.items[0].title} amount={props.items[0].amount}></ExpenseItem>
-            <ExpenseItem date={props.items[1].date} title={props.items[1].title} amount={props.items[1].amount}></ExpenseItem>
-            <ExpenseItem date={props.items[2].date} title={props.items[2].title} amount={props.items[2].amount}></ExpenseItem>
+            {props.items.map(item => (
+                <ExpenseItem date={item.date} title={item.title} amount={item.amount}></ExpenseItem>
+                )
+            )}
         </Card>
     );
 }
