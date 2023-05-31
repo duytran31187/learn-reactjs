dee11c7e8bc2d490a9918e98749bb0662a2c8a00
Duy Tran <duy.tranquoc@gforces.auto>
2023-05-31 21:53:26 +0700

    click event with function as const

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,16 @@ import Card from '../UI/Card';
 const ExpenseItem = (props) => { // jsx just use only one params: props. not as js function(data,title, amount)
     // rule: only one root element
     // do not confuse: $ is dollar
+    const clickHandler = () => {
+        console.log("clicked !!!!");
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <Card className='expense-item__price'>${props.amount}</Card>
-                <button onClick={() => {
-                    console.log('clicked');
-                }}>Change Title</button>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
     )
