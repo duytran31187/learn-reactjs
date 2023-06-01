ec705beb82e4b2e50b89f832c6da3d6c5f331ad7
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-01 09:26:37 +0700

    5: 2-way binding

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -3,12 +3,14 @@ import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const [userInput, setUserInput] = useState({
-
-    'enteredTitle': '',
-    'enteredAmount': '',
-    'enteredDate': '',
-  });
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   'enteredTitle': '',
+  //   'enteredAmount': '',
+  //   'enteredDate': '',
+  // });
 
   const titleChangeHandler = (event) => {
     console.log('Title changed !!!', event.target.value);
@@ -18,9 +20,10 @@ const ExpenseForm = () => {
     //   enteredTitle: event.target.value
     // });
     // with this approach, React ensure merge with latest data
-    setUserInput((prevState) => {
-      return {...prevState, enteredTitle: event.target.value};
-    });
+    // setUserInput((prevState) => {
+    //   return {...prevState, enteredTitle: event.target.value};
+    // });
+    setEnteredTitle(event.target.value);
   };
   const amountChangeHandler = (event) => {
     console.log('amount changed !!!', event.target.value);
@@ -28,9 +31,10 @@ const ExpenseForm = () => {
     //   ...userInput,
     //   enteredAmount: event.target.value
     // });
-    setUserInput((prevState) => {
-      return {...prevState, enteredAmount: event.target.value};
-    });
+    // setUserInput((prevState) => {
+    //   return {...prevState, enteredAmount: event.target.value};
+    // });
+    setEnteredAmount(event.target.value);
   };
   const dateChangeHandler = (event) => {
     console.log('date changed !!!', event.target.value);
@@ -38,19 +42,23 @@ const ExpenseForm = () => {
     //   ...userInput,
     //   enteredDate: event.target.value
     // });
-    setUserInput((prevState) => {
-      return {...prevState, enteredDate: event.target.value};
-    });
+    // setUserInput((prevState) => {
+    //   return {...prevState, enteredDate: event.target.value};
+    // });
+    setEnteredDate(event.target.value);
   };
 
   const submitHandler = (event) => {
     event.preventDefault(); // need it to prevent submit, unless the page will be reloaded
     const expensedata = {
-      title: userInput.enteredTitle,
-      amount: userInput.enteredAmount,
-      date: new Date(userInput.enteredDate)
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
     }
     console.log(expensedata);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   }
 
   return (
@@ -58,15 +66,15 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01'   onChange={amountChangeHandler}/>
+          <input type='number' min='0.01' step='0.01' value={enteredAmount}   onChange={amountChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31'  onChange={dateChangeHandler}/>
+          <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate}  onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className='new-expense__actions'>
