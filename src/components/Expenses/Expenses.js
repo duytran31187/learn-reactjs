846861382dafa19cdca8202c958a9d008c1993ad
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-02 20:46:20 +0700

    s5: add new component: filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+        console.log('filter', selectedYear);
+        setFilteredYear(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter filteredYear={filteredYear} onFilterChanged={filterChangeHandler}></ExpensesFilter>
             {props.items.map(item => (
                 <ExpenseItem key={item.id} date={item.date} title={item.title} amount={item.amount}></ExpenseItem>
                 )
