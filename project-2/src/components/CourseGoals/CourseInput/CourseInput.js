8c83cc5329ac690b462c0f648b8a2aeab7ba172a
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-03 16:29:59 +0700

    s6: dynamic style, class

--- project-2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ project-2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,20 +5,31 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim() === '') {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (enteredValue.trim() !== '') {
+      setIsValid(true);
+      props.onAddGoal(enteredValue);
+    } else {
+      setIsValid(false);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
+      <div className={`form-control ${isValid ? 'valid' : 'invalid'}`}>
+        <label style={{color: isValid ? 'black' : 'orange'}}>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
