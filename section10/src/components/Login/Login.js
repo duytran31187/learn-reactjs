bfbdcfff2d3bd87c6f6082007b570d0f25f1d0fd
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-04 09:37:28 +0700

    s10: effect cleanup

--- section10/src/components/Login/Login.js
+++ section10/src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 2)
+    const identifier = setTimeout(() => {
+      console.log('check form valid!!!');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 2
+      );
+    }, 500);
+    return () => {
+      console.log('clean up');
+      clearTimeout(identifier);
+    }
   }, [enteredEmail, enteredPassword]);
-  
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
   };
