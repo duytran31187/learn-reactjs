2f7b7f867626485122ff870ed45e31e5fc3ebe64
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-06 07:12:01 +0700

    section18: use reduxjs/toolkit, reactREduct

--- section10/src/App.js
+++ section10/src/App.js
@@ -3,9 +3,13 @@ import React, { useState, useEffect} from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import {authActions} from './store/auth';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
   useEffect(() => {
     const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
@@ -19,17 +23,20 @@ function App() {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    dispatch(authActions.login());
   };
 
   const logoutHandler = () => {
     localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
+    dispatch(authActions.logout());
   };
 
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
+        { isAuth && <h2>Logged in with state </h2> }
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
